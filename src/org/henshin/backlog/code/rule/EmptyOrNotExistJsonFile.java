f18be86a366ce1d27bfd1b3104c946ece925d7d8
Amir Rabieyan Nejad <70313484+amirrabieyannejad@users.noreply.github.com>
2024-04-08 12:34:04 +0200

    update tests

--- src/org/henshin/backlog/code/rule/EmptyOrNotExistJsonFile.java
+++ src/org/henshin/backlog/code/rule/EmptyOrNotExistJsonFile.java
@@ -3,7 +3,7 @@ package org.henshin.backlog.code.rule;
 public class EmptyOrNotExistJsonFile extends Exception{
 
 	public EmptyOrNotExistJsonFile() {
-		super("The JOSNObject is empty or not exist!");
+		super("The JOSN File is empty or not exist!");
 	}
 
 }
